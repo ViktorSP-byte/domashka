85cef3f5805ea0e6b5952c6e58c3d5011ecd9e92
Viktor <flystrike1994@gmail.com>
2024-07-30 09:31:16 +0300

    homework 12_2

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    "C:/Users/RobotComp.ru/PycharmProjects/course2_homeworks/logs/masks.log", encoding="utf-8"
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s: %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
 def get_mask_card_number(card_2: str) -> str:
     """Функция, маскирующая номер карты"""
     return card_2.replace(card_2[6:-4], "******")
